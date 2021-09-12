ae01d084d2db31a286af76ed4dbca1300ee33f39
alexeim <alexeim.dev@gmail.com>
2021-09-12 10:01:44 +0300

    Refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -3,20 +3,15 @@ const cors = require('cors');
 const path = require('path');
 const api = require('./api');
 
-const app = express();
+const httpPort = process.env.HTTP_PORT || 5000;
 
-const publicPath = path.join(__dirname, '/../../client/public');
-const buildPath = path.join(__dirname, '/../../client/build');
+const app = express();
 
 app.use(cors());
 app.use(express.static(publicPath));
 
 app.use('/api', api);
-app.use('/', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
-});
 
-const httpPort = process.env.HTTP_PORT || 5000;
 app.listen(httpPort, () => {
 	console.info(`listening on port ${httpPort}`);
 });
