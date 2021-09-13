c0534e5b05b899ffc629877d6ed04ef67f3377ef
alexeim <alexeim.dev@gmail.com>
2021-09-13 10:49:50 +0300

    Update mongo test

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -9,9 +9,9 @@ user.get('/', async (req, res) => {
 })
 
 user.get('/test', async (req, res) => {
-    const conn = mongoose.createConnection("mongodb+srv://alexeimdev:<password>@cluster0.dopsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-    const userModel = conn.model("User", require('../models/schemas/user'))
-    const users = await userModel.find();
+    const conn = mongoose.createConnection("mongodb+srv://alexeimdev:reason10@cluster0.dopsm.mongodb.net/messaging-app?retryWrites=true&w=majority");
+    const UserModel = conn.model("User", require('../models/schemas/user'))
+    const users = await UserModel.find().exec();
 
     res.end();
 })
