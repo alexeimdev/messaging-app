5ed9c6c242cedb39d5326fe6f06f538b3c34487a
alexeim <alexeim.dev@gmail.com>
2021-09-13 23:45:08 +0300

    db + logs

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const usersDb = require('../db/users.json');
+//const db = require('../db/db');
+const usersDbStub = require('../db/stub/users.json');
+const userModel = require('../models/userModel');
 
 const user = express.Router();
 
 user.get('/', async (req, res) => {
-    res.send(usersDb);
+    const users = usersDbStub.users;
+    console.log('[api.user]', 'users', users);
+    res.send(users);
 })
 
 user.get('/test', async (req, res) => {
-    const conn = mongoose.createConnection("mongodb+srv://alexeimdev:reason10@cluster0.dopsm.mongodb.net/messaging-app?retryWrites=true&w=majority");
-    const UserModel = conn.model("User", require('../models/schemas/user'))
-    const users = await UserModel.find().exec();
-
-    res.end();
+    try {
+        const users = await userModel.find();
+        console.log('[api.user.test]', 'users', users);
+        res.send(users);
+    } catch (error) {
+        console.error('[api.user.test]', error);
+        res.sendStatus(500);
+    }
 })
 
 user.get('/:id', (req, res) => {
-    res.send(usersDb.users.find(x => x.email == req.params.id));
+    const user = usersDbStub.users.find(x => x.email == req.params.id);
+    console.debug('[api.user.id]', 'user', user);
+    res.send(user);
 })
-
-
 
 module.exports = user;
