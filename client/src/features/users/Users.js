8e69f991344869f82fea5ad783bfeda37d700208
alexeim <alexeim.dev@gmail.com>
2021-09-18 20:24:30 +0300

    Added select user

--- client/src/features/users/Users.js
+++ client/src/features/users/Users.js
@@ -3,21 +3,29 @@ import styles from './Users.module.css';
 
 export function Users() {
     const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState("");
 
     useEffect(() => {
         fetchUsers();
+
         async function fetchUsers() {
             const response = await fetch('/api/user');
             const users = await response.json();
             setUsers(users);
+            setSelectedUser(users[0].email);
         }
     }, []);
 
+    function handleSelectUserChange (e) {
+        setSelectedUser(e.target.value);
+    }
+
     return (
         <div className={styles.users}>
-            <select className={styles.dropdown}>
+            <select onChange={handleSelectUserChange} className={styles.dropdown} >
                 {users?.map(user => <option>{user.email}</option>)}
             </select>
+            <h2>{selectedUser}</h2>
         </div>
     )
 }
