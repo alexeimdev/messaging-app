6f7a9fcb116891f91c7dc8f46a9767a52ff3e0fe
alexeim <alexeim.dev@gmail.com>
2021-09-21 19:24:13 +0300

    Added time props and styles

--- client/src/features/chat/Message.js
+++ client/src/features/chat/Message.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import styles from './Message.module.css';
+import styles from './Message.module.scss';
 
 export function Message(props) {
     return (
         <div className={`${styles.message} ${props.me ? styles.me : ''}`}>
-            <div className="text">{props.text}</div>
+            <div className={styles.text}>{props.text}</div>
+            <div className={styles.time}>{props.time}</div>
         </div>
     )
 }
