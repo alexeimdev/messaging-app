eafce481539e0337ba22529d384465320b2f575a
alexeim <alexeim.dev@gmail.com>
2021-09-21 23:03:03 +0300

    UI improvment

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -6,20 +6,23 @@ import { Date } from './Date';
 export function Chat(props) {
     return (
         <div className={styles.chat}>
-            <Date date="Yesterday" />
-            <Message text="Hi!" time="07:03" me />
-            <Message text="How you doing?" time="07:05" />
-            <Message text="I'm fine thanks. How are you?" time="07:05" me />
-            <Message text="Execllent" time="07:07" />
-            <Message text="Hi!" time="07:03" me />
-            <Date date="Today" />
-            <Message text="How you doing?" time="07:05" />
-            <Message text="I'm fine thanks. How are you?" time="07:05" me />
-            <Message text="Execllent" time="07:07" />
-            <Message text="Hi!" time="07:03" me />
-            <Message text="How you doing?" time="07:05" />
-            <Message text="I'm fine thanks. How are you?" time="07:05" me />
-            <Message text="Execllent" time="07:07" />
+            <div className={styles.bg} />
+            <div className={styles.messages}>
+                <Date date="Yesterday" />
+                <Message text="Hi!" time="07:03" me />
+                <Message text="How you doing?" time="07:05" />
+                <Message text="I'm fine thanks. How are you?" time="07:05" me />
+                <Message text="Execllent" time="07:07" />
+                <Message text="Hi!" time="07:03" me />
+                <Date date="Today" />
+                <Message text="How you doing?" time="07:05" />
+                <Message text="I'm fine thanks. How are you?" time="07:05" me />
+                <Message text="Execllent" time="07:07" />
+                <Message text="Hi!" time="07:03" me />
+                <Message text="How you doing?" time="07:05" />
+                <Message text="I'm fine thanks. How are you?" time="07:05" me />
+                <Message text="Execllent" time="07:07" />
+            </div>
         </div>
     )
 }
