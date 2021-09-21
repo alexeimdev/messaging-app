b841cebc3ae6db141d0471252c809e0ef537b480
alexeim <alexeim.dev@gmail.com>
2021-09-21 21:54:40 +0300

    Converted .css files to .scss + Added base.scss

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Chat.module.css';
+import styles from './Chat.module.scss';
 import { Message } from './Message';
 
 export function Chat(props) {
--- client/src/layout/layout.js
+++ client/src/layout/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../layout/layout.module.css';
+import styles from '../layout/layout.module.scss';
 
 export function Layout(props) {
     return (
