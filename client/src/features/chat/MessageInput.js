27ba795197995f063e8913896eeaba24e09fe45e
alexeim <alexeim.dev@gmail.com>
2021-09-22 17:24:13 +0300

    Changed to form

--- client/src/features/chat/MessageInput.js
+++ client/src/features/chat/MessageInput.js
@@ -2,11 +2,20 @@ import React from 'react';
 import styles from './MessageInput.module.scss';
 
 export function MessageInput(props) {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (e.target[0].value) {
+            props.onSubmit(e.target[0].value);
+            e.target.reset();
+        }
+    }
+
     return (
-        <div className={styles.messageInputWrapper}>
+        <form onSubmit={handleSubmit} className={styles.messageInputForm}>
             <input className={styles.messageInput} />
             <div className={styles.spacer} />
-            <input type="button" className={styles.submitButton} />
-        </div>
+            <input type="submit" value="" className={styles.submitButton} />
+        </form>
     )
 }
