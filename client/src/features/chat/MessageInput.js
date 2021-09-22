4385eab6626e4f5710451adf607535ac4d3381ba
alexeim <alexeim.dev@gmail.com>
2021-09-22 18:07:05 +0300

    Changed Input

--- client/src/features/chat/MessageInput.js
+++ client/src/features/chat/MessageInput.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from './MessageInput.module.scss';
 
 export function MessageInput(props) {
 
+    const inputRef = useRef();
+
     function handleSubmit(e) {
         e.preventDefault();
-        if (e.target[0].value) {
-            props.onSubmit(e.target[0].value);
-            e.target.reset();
+        if (inputRef.current.value) {
+            props.onSubmit(inputRef.current.value);
+            inputRef.current.value = "";
+            inputRef.current.focus();
         }
     }
 
     return (
         <form onSubmit={handleSubmit} className={styles.messageInputForm}>
-            <input className={styles.messageInput} />
+            <input ref={inputRef} className={styles.messageInput} />
             <div className={styles.spacer} />
-            <input type="submit" value="" className={styles.submitButton} />
+            <button type="submit" value="" className={styles.submitButton} />
         </form>
     )
 }
