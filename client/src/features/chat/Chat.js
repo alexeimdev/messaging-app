80625d8e3a8e3e36aa5c033fe2dbdb80e12ae36b
alexeim <alexeim.dev@gmail.com>
2021-09-22 08:13:15 +0300

    Cleanup

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -57,18 +57,3 @@ export function Chat(props) {
         </div>
     )
 }
-
-{/* <Date date="Yesterday" />
-<Message text="Hi!" time="07:03" me />
-<Message text="How you doing?" time="07:05" />
-<Message text="I'm fine thanks. How are you?" time="07:05" me />
-<Message text="Execllent" time="07:07" />
-<Message text="Hi!" time="07:03" me />
-<Date date="Today" />
-<Message text="How you doing?" time="07:05" />
-<Message text="I'm fine thanks. How are you?" time="07:05" me />
-<Message text="Execllent" time="07:07" />
-<Message text="Hi!" time="07:03" me />
-<Message text="How you doing?" time="07:05" />
-<Message text="I'm fine thanks. How are you?" time="07:05" me />
-<Message text="Execllent" time="07:07" /> */}
