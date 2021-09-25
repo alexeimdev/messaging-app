6b3343367c054d5bda596d578cfddd5c114167a1
alexeim <alexeim.dev@gmail.com>
2021-09-25 15:54:19 +0300

    Using redux-toolkit

--- client/src/features/chat/Chat.js
+++ client/src/features/chat/Chat.js
@@ -1,78 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Chat.module.scss';
 import { Messages } from "./Messages";
 import { MessageInput } from "./MessageInput";
 import { Modal } from "../../shared/components/Modal";
 import { ConnectedUser } from '../users/ConnectedUser';
+import { addMessage } from './chatSlice';
 
 export function Chat(props) {
 
-    const [messages, setMessages] = useState([]);
     const [user, setUser] = useState('');
-
-    // useEffect(() => {
-
-    //     const today = new Date();
-    //     const yesterday = today.getDate() - 1;
-    //     const tomorrow = today.getDate() + 1;
-
-    //     setMessages([
-    //         {
-    //             date: "Yesterday",
-    //             messagesArr: [
-    //                 { time: "07:03", text: "Hi!", author: "me" },
-    //                 { time: "07:05", text: "How you doing?", author: "liat5861@gmail.com" },
-    //                 { time: "07:06", text: "I'm fine thanks. How are you?", author: "me" },
-    //                 { time: "07:10", text: "Execllent", author: "liat5861@gmail.com" },
-    //             ]
-    //         },
-    //         {
-    //             date: "Today",
-    //             messagesArr: [
-    //                 { time: "07:03", text: "Hi!", author: "me" },
-    //                 { time: "07:05", text: "How you doing?", author: "liat5861@gmail.com" },
-    //                 { time: "07:06", text: "I'm fine thanks. How are you?", author: "me" },
-    //                 { time: "07:10", text: "Execllent", author: "liat5861@gmail.com" },
-    //                 { time: "07:11", text: "😃", author: "me" },
-    //             ]
-    //         },
-    //     ]);
-
-    // }, []);
+    const messages = useSelector(state => state.chat.messages);
 
     useEffect(() => {
-
         props.onIncomingMassage(addMessage);
-
-        function addMessage({author, message}) {
-            const now = new Date();
-            const currentHour = ("0" + now.getHours()).slice(-2);
-            const currentMinutes = ("0" + now.getMinutes()).slice(-2);
-    
-            let newMessages = [...messages];
-            let todayMessages = newMessages?.find(x => x.date === "Today");
-    
-            if (todayMessages) {
-                newMessages.find(x => x.date === "Today").messagesArr.push({
-                    time: `${currentHour}:${currentMinutes}`,
-                    author: author,
-                    message: message,
-                });
-            } 
-            else {
-                newMessages.push({
-                    date: "Today",
-                    messagesArr: [{
-                        time: `${currentHour}:${currentMinutes}`,
-                        author: author,
-                        message: message,
-                    }]
-                });
-            }
-    
-            setMessages(newMessages);
-        }
-
     }, [props.onIncomingMassage]);
 
     function createNewMessage(message) {
@@ -90,7 +31,7 @@ export function Chat(props) {
             <Messages me={user} messages={messages} />
             <MessageInput onSubmit={createNewMessage} />
             <Modal title="Choose user" show={!user} hideCloseButton>
-                <ConnectedUser users={['alexei@test.com', 'liat@test.com']} onSelectUser={(user) => setUser(user)} />
+                <ConnectedUser users={['alexei@example.com', 'liat@example.com']} onSelectUser={(user) => setUser(user)} />
             </Modal>
         </div>
     )
