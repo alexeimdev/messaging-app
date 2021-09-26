81c33759bcce8bff12309dc0bf9b2674d244de7a
alexeim <alexeim.dev@gmail.com>
2021-09-26 19:21:14 +0300

    Changed socket state to socket ref

--- client/src/hooks/chatHook.js
+++ client/src/hooks/chatHook.js
@@ -1,30 +1,29 @@
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import io from "socket.io-client";
 
 export function useChat() {
 
-    const [socket, setSocket] = useState(null);
+    const socketRef = useRef();
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (socket == null) {
-            setSocket(io.connect());
+        if (socketRef.current == null) {
+            socketRef.current = io.connect();
         }
-        return () => {
-        }
-    }, [])
+        // return () => { }
+    }, [socketRef.current])
 
     function startChat(chatId) {
         console.log("startChat", 'chatId', chatId);
-        socket?.emit("chat", {
+        socketRef.current?.emit("chat", {
             chatId: chatId,
         });
     }
 
     function sendMessage({ author, message }) {
         console.log("sendMessage", 'author', author, 'message', message);
-        socket?.emit("message", {
+        socketRef.current?.emit("message", {
             author: author,
             message: message,
         });
@@ -32,17 +31,23 @@ export function useChat() {
 
     function onIncomingMassage(action) {
         console.log("onIncomingMassage");
-        socket?.on("message", (incomingMessage) => {
+        socketRef.current?.on("message", (incomingMessage) => {
             console.log("on message", 'incomingMessage', incomingMessage);
             dispatch(action(incomingMessage));
         });
     }
 
     function onGetChatHistory(action) {
-        socket?.on("chatHistory", (messages) => {
+        socketRef.current?.on("chatHistory", (messages) => {
             dispatch(action(messages));
         });
     }
 
-    return { startChat, sendMessage, onIncomingMassage, onGetChatHistory };
+    return {
+        startChat,
+        sendMessage,
+        onIncomingMassage,
+        onGetChatHistory
+    }
+
 }
