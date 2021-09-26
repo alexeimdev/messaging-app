5ce801a125d3dcae27fdc66eae5a034e12f6449a
alexeim <alexeim.dev@gmail.com>
2021-09-26 12:28:20 +0300

    Added sub title

--- client/src/layouts/chatLayout.js
+++ client/src/layouts/chatLayout.js
@@ -6,6 +6,9 @@ export function ChatLayout(props) {
         <div className={styles.layout}>
             <header className={styles.header}>
                 <h1 className={styles.headerTitle}>{props.headerTitle}</h1>
+                {props.headerSubTitle &&
+                    <h2 className={styles.headerSubTitle}>{props.headerSubTitle}</h2>
+                }
             </header>
             <main className={styles.main}>{props.children}</main>
             <footer className={styles.footer}></footer>
