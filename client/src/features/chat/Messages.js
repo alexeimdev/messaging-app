31ffa71809e12603b4b5119ae1ad1197f80c921c
alexeim <alexeim.dev@gmail.com>
2021-09-28 21:46:29 +0300

    Grouping messages

--- client/src/features/chat/Messages.js
+++ client/src/features/chat/Messages.js
@@ -20,22 +20,41 @@ export function Messages(props) {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }
 
-    const groupedMessages = [];
-    if (props.messages) {
-        const now = new Date();
+    const groupedMessages = groupMessages(props.messages);
 
-        for (const message of props.messages) {
+    function groupMessages(messages) {
+        const groupedMessages = [];
+        for (const message of messages) {
             const messageDate = new Date(message.date);
-            const shortDate = (messageDate.toLocaleDateString() === now.toLocaleDateString())
-                ? "Today" : messageDate.toLocaleDateString();
+            const shortDate = getDisplayDate(messageDate);
 
             const existingDate = groupedMessages?.find(group => group.date === shortDate)?.date;
             if (existingDate) {
                 groupedMessages.find(group => group.date === shortDate).messages.push(message);
-            } 
+            }
             else {
                 groupedMessages.push({ date: shortDate, messages: [message] })
             }
+        }
+        return groupedMessages;
+    }
+
+    function getDisplayDate(date) {
+        const now = new Date();
+        const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+
+        const isToday = now.toLocaleDateString() === date.toLocaleDateString();
+        const isYesterday = yesterday.toLocaleDateString() === date.toLocaleDateString();
+
+        const months = ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'];
+
+        if (isToday) {
+            return "Today";
+        } else if (isYesterday) {
+            return "Yesterday";
+        } else {
+            return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
         }
     }
 
@@ -45,7 +64,7 @@ export function Messages(props) {
             {groupedMessages?.map((groupItem, groupindex) =>
                 <React.Fragment key={groupindex}>
                     <MessagesDate date={groupItem.date} />
-                    {groupItem.messages.map((messageItem, messageIndex, messageArr) => 
+                    {groupItem.messages.map((messageItem, messageIndex, messageArr) =>
                         <Message key={messageIndex}
                             message={messageItem.message}
                             date={messageItem.date}
